f0a937249782d7fdbcfc874b1f5c9633884d29f8
rodogi <rodgdor@gmail.com>
2019-12-04 12:56:25 -0600

    add go to def in jedi

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;; Configure package.el to include MELPA.
+;http://www.nochedemuseos.cdmx.gob.mx/#image-19; Configure package.el to include MELPA.
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -18,15 +18,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-	 (quote
-		("/Users/rdora/Dropbox/org/cool_blogs.org" "/Users/rdora/Dropbox/org/inmegen.org" "/Users/rdora/Dropbox/org/life.org" "/Users/rdora/Dropbox/org/mobile.org" "/Users/rdora/Dropbox/org/network_science.org")))
  '(package-selected-packages
 	 (quote
-		(ox-reveal projectile flycheck evil-escape zenburn-theme zenburn auto-compile use-package solarized-theme py-autopep8 popwin ox-twbs ox-hugo org-ref org-bullets neotree moody minions magit-popup magit linum-relative jedi header2 haskell-mode ghub fill-column-indicator exec-path-from-shell evil-visualstar evil-vimish-fold evil-surround evil-leader ess elpy ein diff-hl avy anzu all-the-icons))))
+		(ox-reveal evil-escape zenburn-theme zenburn auto-compile use-package solarized-theme py-autopep8 popwin ox-twbs ox-hugo org-ref org-bullets neotree moody minions magit-popup magit linum-relative jedi header2 haskell-mode ghub fill-column-indicator exec-path-from-shell evil-visualstar evil-vimish-fold evil-surround evil-leader ess elpy ein diff-hl avy anzu all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F")))))
+ '(default ((t (:family "IBM Plex Mono" :foundry "nil" :slant normal :weight normal :height 140 :width normal)))))
